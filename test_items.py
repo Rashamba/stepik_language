82a5e26a30e7713ca75fcedf5787476ef854baf5
Rashamba <rashamba528@gmail.com>
2023-04-02 00:21:38 +0300

    Add time sleep

--- test_items.py
+++ test_items.py
@@ -7,6 +7,7 @@ url = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
 
 def test_button_add(browser):
     browser.get(url)
+    time.sleep(10)
     button_add_to_cart = browser.find_element(By.XPATH, "//button[@class='btn btn-lg btn-primary btn-add-to-basket']")
     assert button_add_to_cart, "Button not found"
-    time.sleep(5)
+
